98128a78f3b42b76c9282b62c04c0b7ea59c4261
sandrineedrina <sandrineedrina@gmail.com>
2025-07-30 22:52:32 +0200

    refresh

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -4,4 +4,17 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://ujswkxqlkeyegananzqn.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqc3dreHFsa2V5ZWdhbmFuenFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2MTkzNzYsImV4cCI6MjA2OTE5NTM3Nn0.8xIhBPiZMxdrPcyQHOp4Y1AWhaJu9LErfc6DK4eoE1o'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+// Options de configuration recommandées
+const supabaseOptions = {
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true
+  },
+  global: {
+    headers: { 'x-application-name': 'MonApplication' }
+  }
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, supabaseOptions)
